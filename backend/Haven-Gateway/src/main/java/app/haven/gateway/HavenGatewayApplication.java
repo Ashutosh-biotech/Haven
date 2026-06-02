2c64719ba9e4215c7bd88c943039885f0c6ff77d
Ashutosh Kumar <ashutoshbiotech2020@gmail.com>
2026-06-02 13:10:23 +0530

    added api gateway

---
+++ backend/Haven-Gateway/src/main/java/app/haven/gateway/HavenGatewayApplication.java
@@ -0,0 +1,13 @@
+package app.haven.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HavenGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HavenGatewayApplication.class, args);
+	}
+
+}
---
+++ backend/Haven-Gateway/src/test/java/app/haven/gateway/HavenGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package app.haven.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HavenGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
