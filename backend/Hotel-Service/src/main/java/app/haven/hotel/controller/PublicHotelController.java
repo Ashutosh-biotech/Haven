9c8f1cb129ac2efe9b78ddf431daef040bedc22c
Ashutosh Kumar <ashutoshbiotech2020@gmail.com>
2026-06-30 14:53:10 +0530

    Update application.yml, HotelController.java, and PublicHotelController.java

--- backend/Hotel-Service/src/main/java/app/haven/hotel/controller/PublicHotelController.java
+++ backend/Hotel-Service/src/main/java/app/haven/hotel/controller/PublicHotelController.java
@@ -1,12 +1,11 @@
 package app.haven.hotel.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/hotel")
-public class HotelController {
+@RequestMapping("/api/v1/public/hotel")
+public class PublicHotelController {
 
 
 
