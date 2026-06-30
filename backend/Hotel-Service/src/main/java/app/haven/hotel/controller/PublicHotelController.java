ac3f9bdad40a16eb3eda91010a05d51698e5fe71
Ashutosh Kumar <ashutoshbiotech2020@gmail.com>
2026-06-30 16:57:33 +0530

    Update PublicHotelController.java

--- backend/Hotel-Service/src/main/java/app/haven/hotel/controller/PublicHotelController.java
+++ backend/Hotel-Service/src/main/java/app/haven/hotel/controller/PublicHotelController.java
@@ -1,12 +1,19 @@
 package app.haven.hotel.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/public/hotel")
 public class PublicHotelController {
 
-
+    @GetMapping("/")
+    public ResponseEntity<?> getPublicHotel(){
+        return ResponseEntity.ok(Map.of("status", "This is hotel service"));
+    }
 
 }
