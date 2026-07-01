8a4a9eb525a9c93217f0ff37e76b08f214c8426b
Ashutosh Kumar <ashutoshbiotech2020@gmail.com>
2026-07-01 16:07:34 +0530

    Update PublicHotelController.java

--- backend/Hotel-Service/src/main/java/app/haven/hotel/controller/PublicHotelController.java
+++ backend/Hotel-Service/src/main/java/app/haven/hotel/controller/PublicHotelController.java
@@ -1,19 +1,31 @@
 package app.haven.hotel.controller;
 
+import app.haven.hotel.models.entity.Hotel;
+import app.haven.hotel.service.HotelService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/public/hotel")
 public class PublicHotelController {
 
+    private final HotelService hotelService;
+
+    PublicHotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
     @GetMapping("/")
-    public ResponseEntity<?> getPublicHotel(){
-        return ResponseEntity.ok(Map.of("status", "This is hotel service"));
+    public ResponseEntity<Page<Hotel>> getHotelsByPage(@RequestParam(defaultValue = "0") int page){
+        Pageable pageable = PageRequest.of(page, 10);
+        Page<Hotel> hotels = hotelService.getHotelsByPage(pageable);
+        return ResponseEntity.ok(hotels);
     }
 
 }
