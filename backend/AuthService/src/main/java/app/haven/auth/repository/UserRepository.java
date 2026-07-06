4889bf91cb8e951b6229657ee315c879baca0add
Ashutosh Kumar <ashutoshbiotech2020@gmail.com>
2026-07-06 20:20:26 +0530

    Update UserRepository.java

--- backend/AuthService/src/main/java/app/haven/auth/repository/UserRepository.java
+++ backend/AuthService/src/main/java/app/haven/auth/repository/UserRepository.java
@@ -2,6 +2,10 @@ package app.haven.auth.repository;
 
 import app.haven.auth.models.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
