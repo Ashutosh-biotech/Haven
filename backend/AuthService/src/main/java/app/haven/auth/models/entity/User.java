ade2052fe4215076be6f2b8b095c6acdfa8871e5
Ashutosh Kumar <ashutoshbiotech2020@gmail.com>
2026-07-12 09:42:55 +0530

    Update SecurityConfig.java, User.java, and PlatformRoleRepository.java

--- backend/AuthService/src/main/java/app/haven/auth/models/entity/User.java
+++ backend/AuthService/src/main/java/app/haven/auth/models/entity/User.java
@@ -14,6 +14,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -81,6 +82,7 @@ public class User {
     private String displayName;
 
     @NotBlank
+    @Email
     @Column(nullable = false, unique = true)
     private String email;
 
--- backend/AuthService/src/main/java/app/haven/auth/repository/PlatformRoleRepository.java
+++ backend/AuthService/src/main/java/app/haven/auth/repository/PlatformRoleRepository.java
@@ -1,11 +1,15 @@
 package app.haven.auth.repository;
 
 import app.haven.auth.models.entity.PlatformRole;
+import app.haven.auth.models.enums.PlatformRoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PlatformRoleRepository extends JpaRepository<PlatformRole, UUID> {
+    Optional<PlatformRole> findByName(PlatformRoleEnum name);
 }
+
