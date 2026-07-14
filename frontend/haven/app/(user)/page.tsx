269932281c215f3ff4b4d1b2dd4cbedc89f0174a
Ashutosh Kumar <ashutoshbiotech2020@gmail.com>
2026-07-14 22:34:34 +0530

    file name changes !!!

--- frontend/haven/app/(user)/page.tsx
+++ frontend/haven/app/(user)/page.tsx
@@ -1,8 +1,8 @@
 import { DestinationsCarousel } from "@/components/ui/home/DestinationsCarousel";
-import { FeaturedHotels } from "@/components/ui/home/FeaturedHotels";
-import { Header } from "@/components/ui/home/Header";
-import HowItWorks from "@/components/ui/home/HowItWorks";
-import TestimonialsCarousel from "@/components/ui/home/TestimonialsCarousel";
+import { FeaturedHotels } from "@/components/ui/home/featured-hotels";
+import { Header } from "@/components/ui/home/header";
+import HowItWorks from "@/components/ui/home/how-it-works";
+import TestimonialsCarousel from "@/components/ui/home/testimonials-carousel";
 import React from "react";
 
 export default function Home(): React.ReactNode {
