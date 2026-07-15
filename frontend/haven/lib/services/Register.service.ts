8094e82470d67fc52fc48efd99b949ebccea849d
Ashutosh Kumar <ashutoshbiotech2020@gmail.com>
2026-07-15 13:09:53 +0530

    Update Register.service.ts

--- frontend/haven/lib/services/Register.service.ts
+++ frontend/haven/lib/services/Register.service.ts
@@ -1,5 +1,12 @@
-function RegisterUser():boolean {
+import axios from "axios";
+import {BackendRoutes} from "@/router/routes";
+import {UserRegistrationFormData} from "@/components/interface/user-registration-form-data";
 
+async function RegisterUser(jsonData:UserRegistrationFormData): Promise<boolean> {
+    await axios.post(BackendRoutes.register(), {
+        headers: {},
+        body: JSON.stringify(jsonData)
+    });
     return true;
 }
 
