e4dcefdaea5940f4790970c4f2e97414f3ae5d4e
Ashutosh Kumar <ashutoshbiotech2020@gmail.com>
2026-07-15 13:09:56 +0530

    Update routes.ts

--- frontend/haven/router/routes.ts
+++ frontend/haven/router/routes.ts
@@ -3,13 +3,22 @@ type routesType = {
 }
 
 const Routes: routesType = {
-    home: () => "/",
-    cities: () => "/cities",
-    city: (slug: string) => `/city/${slug}`,
-    hotels: () => "/hotels",
-    hotel: (slug: string) => `/hotel/${slug}`,
-    login: () => "/login",
-    register: () => "/register",
+    home: () :string => "/",
+    cities: () :string => "/cities",
+    city: (slug: string) :string => `/city/${slug}`,
+    hotels: () :string => "/hotels",
+    hotel: (slug: string) :string => `/hotel/${slug}`,
+    login: () :string => "/login",
+    register: () :string => "/register",
+}
+
+const backendBaseRoute = "http://localhost:8080/api/v1";
+
+const BackendRoutes: routesType = {
+
+    register: () :string => backendBaseRoute+"/public/register"
+
 }
 
 export default Routes;
+export {BackendRoutes};
